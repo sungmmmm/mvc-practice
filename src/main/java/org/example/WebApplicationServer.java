a530a2d389ccbba3024d257117cd95c7b7e336eb
sungmmmm <sadlzn77@gmail.com>
2023-06-09 22:45:35 +0900

    Initial commit

---
+++ src/main/java/org/example/WebApplicationServer.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class WebApplicationServer {
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapps/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
